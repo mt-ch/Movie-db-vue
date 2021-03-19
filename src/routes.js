d070e17dd982489ed29e4c845148f5cab026a9fd
mt-ch <matthew.chan@icloud.com>
2021-03-19 03:19:47 +0000

    route fix

--- src/routes.js
+++ src/routes.js
@@ -6,23 +6,21 @@ Vue.use(VueRouter);
 import SingleMovie from "./components/movie/SingleMovie.vue";
 import Home from "./components/home/Home.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home,
-  },
-  {
-    path: "/movie/:id",
-    name: "movie",
-    component: SingleMovie,
-  },
-];
-
 const router = new VueRouter({
   mode: "history",
   linkActiveClass: "active",
-  routes: routes,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/movie/:id",
+      name: "movie",
+      component: SingleMovie,
+    },
+  ],
 });
 
 export default router;
